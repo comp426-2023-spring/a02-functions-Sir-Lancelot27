860e7b74d380ca3c502823400320e5f7291b9a3a
Sir-Lancelot27 <whitleylance@gmail.com>
2023-03-30 03:42:22 -0400

    Fixed comments

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 import moment from "moment-timezone";
 import minimist from "minimist";
 
-// Create a help string
+// Create a help string.
 const helpText = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
@@ -11,10 +11,10 @@ const helpText = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
     -j            Echo pretty JSON from open-meteo API and exit.`;
 
-// Get arguments from CMD
+// Get arguments from CMD.
 const args = minimist(process.argv.slice(2));
 
-// Process options
+// Process options.
 if (args.h) {
     console.log(helpText);
     process.exit(0);
@@ -29,25 +29,25 @@ const day = args.d || 1;
 
 const json = args.j;
 
-// Make a request
+// Make a request.
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+long+'&timezone='+timezone+'&daily=precipitation_hours');
 
-// Get the data from the request
+// Get the data from the request.
 const data = await response.json();
 
-// If json only return json
+// If json only return json.
 if (json) {
     console.log(data);
     process.exit(0);
 }
 
-// Get what day to report for
+// Get what day to report for.
 const days = args.d 
 
-// Check if need your galoshes
+// Check if need your galoshes.
 var needGaloshes = data.daily.precipitation_hours[day] > 0 ? 'You might need your galoshes ' : 'You will not need your galoshes ';
 
-// Report for given day
+// Report for given day.
 if (days == 0) {
   console.log(needGaloshes + "today.")
 } else if (days > 1) {
